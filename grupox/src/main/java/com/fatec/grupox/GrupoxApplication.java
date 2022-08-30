9f1424fa07c19d8a46246227779e0ffbe79933ba
almei <almei@laptopp>
2022-08-30 15:48:49 -0300

    versao inicial

---
+++ grupox/src/main/java/com/fatec/grupox/GrupoxApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.grupox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoxApplication.class, args);
+	}
+
+}
