a0ed43b439b09ad448856903da900dc3f4f24a3c
dti <dti@LAB204-20>
2022-09-06 11:44:11 -0300

    Configuracao Heroku - Inclusão das classes de segurança

--- grupox/src/main/java/com/fatec/grupox/GrupoxApplication.java
+++ grupox/src/main/java/com/fatec/grupox/GrupoxApplication.java
@@ -2,12 +2,14 @@ package com.fatec.grupox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class GrupoxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrupoxApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("123"));
 	}
 
 }
